bd50eab18a7f181f3910db44856341bc932196f2
shailendra98k <shailendra98k@gmail.com>
2023-07-21 16:40:55 +0530

    Refactored and remove extra tfs directory

--- file-server/tfs/asgi.py
+++ file-server/tfs/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for tfs project.
+ASGI config for tfs-1 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tfs.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tfs-1.settings')
 
 application = get_asgi_application()
