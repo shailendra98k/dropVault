c9ae74d585b29dbfef769068e7bd388624a7d3b5
shailendra98k <shailendra98k@gmail.com>
2023-11-05 12:01:57 +0530

    created context file DefaultContext.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,5 @@
-import React, { useContext, useEffect } from "react";
+import React, { useEffect } from "react";
 import Box from "@mui/material/Box";
-import { AppContext } from "../App";
 import { API_URI, SESSION_ITEMS, URL_PATHS } from "../constants";
 import axios from "axios";
 import FolderCard from "../components/card/FolderCard";
@@ -9,10 +8,11 @@ import { useHistory } from "react-router-dom";
 import Dropbox from "../components/Dropbox";
 import { ActionBar } from "../components/actionBar/ActionBar";
 import { TSFixMe } from "../../types";
+import { useDefaultContext } from "../context/DefaultContext";
 
 const Home = () => {
   const history = useHistory();
-  const { currDir, user, setFiles, setDirectories } = useContext(AppContext);
+  const { currDir, user, files, setFiles, setDirectories } = useDefaultContext();
 
   useEffect(() => {
     if (!sessionStorage.getItem(SESSION_ITEMS.USER)) {
@@ -20,7 +20,7 @@ const Home = () => {
     } else {
       const formData = new FormData();
       formData.append("current_dir", currDir);
-      formData.append("user_id", user?.id);
+      formData.append("user_id", user?.id.toString());
       axios.post(API_URI, formData).then((res) => {
         setDirectories(res.data.sub_dirs);
         setFiles(res.data.files);
@@ -38,13 +38,13 @@ const Home = () => {
 };
 
 const Body = () => {
-  const { files, user, directories } = useContext(AppContext);
+  const { files, user, directories } = useDefaultContext();
 
   return (
     <Box style={{ flexGrow: 4, height: "90vh", overflow: "scroll" }}>
       <Box sx={{ display: "flex", flexWrap: "wrap" }}>
         {directories.map((dir: TSFixMe) => {
-          return <FolderCard data={dir} userid={user.id} />;
+          return <FolderCard data={dir} userid={user.id.toString()} />;
         })}
       </Box>
 
