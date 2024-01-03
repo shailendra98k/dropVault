f5f115e95c3cb219418bf6a0f0c96f6ac4a437f1
shailendra98k <shailendra98k@gmail.com>
2024-01-03 22:18:47 +0530

    Switched to DropVault | used https protocol

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,5 +1,5 @@
-export const API_URI = (process.env.NODE_ENV === 'development') ? 'http://localhost/api/v1' : 'http://dropvault.fun/api/v1' ;
-export const BASE_URI = (process.env.NODE_ENV === 'development') ? 'http://localhost' : 'http://dropvault.fun'
+export const API_URI = (process.env.NODE_ENV === 'development') ? 'http://localhost/api/v1' : 'https://dropvault.fun/api/v1' ;
+export const BASE_URI = (process.env.NODE_ENV === 'development') ? 'http://localhost' : 'https://dropvault.fun'
 
 export const URL_PATHS = {
     HOME : "/",
